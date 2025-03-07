26b282c00c29093afac5638077cfbf0c592302e2
chinomyint1 <1000041084@mcdmstudent.dk>
2025-03-07 12:10:37 +0100

    f

--- js/products.js
+++ js/products.js
@@ -1,10 +1,13 @@
 import { fetchProducts } from "./fetch_data.js";
+import { productListTemplate } from "./templates.js";
 
 const products = await fetchProducts()
 
 export const productlist = () => {
     console.log(products)
     const productListContainer = document.querySelector('.product-list-container')
+
+if(!productListContainer) return;
 
     products.forEach(element => {
         productListContainer.insertAdjacentHTML('beforeend', productListTemplate(element))
